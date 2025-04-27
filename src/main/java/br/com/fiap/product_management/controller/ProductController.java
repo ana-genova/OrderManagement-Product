89e4db80570265d53c8fecf22ba1b3298add033c
Ana Genova <anagenova9@outlook.com>
2025-04-27 15:43:29 -0300

    Adding Swagger and Open Feign

--- src/main/java/br/com/fiap/product_management/controller/ProductController.java
+++ src/main/java/br/com/fiap/product_management/controller/ProductController.java
@@ -5,6 +5,9 @@ import br.com.fiap.product_management.domain.Product;
 import br.com.fiap.product_management.gateway.database.jpa.ProductJpaGateway;
 import br.com.fiap.product_management.usecase.CreateProductUseCase;
 import br.com.fiap.product_management.usecase.UpdateProductUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +22,57 @@ public class ProductController {
         this.productJpaGateway = productJpaGateway;
     }
 
+    @Operation(
+            summary = "Buscar todos produtos",
+            description = """
+                    A chamada deste endpoint lista todos os produtos cadastrados
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listagem com todos os produtos cadastrados"),
+    })
     @GetMapping
     public List<ProductDto> getProducts() {
         List<Product> products = productJpaGateway.findAll();
-        return products.stream().map(ProductDto::new).toList() ;
+        return products.stream().map(ProductDto::new).toList();
     }
 
+    @Operation(
+            summary = "Busca produtos por código",
+            description = """
+                    A chamada deste endpoint lista um produto específico a partir do ID
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listagem de um produto a partir do ID"),
+            @ApiResponse(responseCode = "500", description = "Erro nas informações inseridas")
+    })
     @GetMapping("/{id}")
-    public ProductDto getProductBySKU(@PathVariable("id") Long id) {
+    public ProductDto getProductById(@PathVariable("id") Long id) {
         return new ProductDto(productJpaGateway.findById(id));
     }
 
+    @Operation(
+            summary = "Busca produtos por código SKU",
+            description = """
+                    A chamada deste endpoint lista um produto específico a partir do SKU
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listagem de um produto a partir do SKU"),
+            @ApiResponse(responseCode = "500", description = "Erro nas informações inseridas")
+    })
     @GetMapping("/sku/{sku}")
     public ProductDto getProductBySKU(@PathVariable("sku") String sku) {
         return new ProductDto(productJpaGateway.findBySKU(sku));
     }
 
+    @Operation(
+            summary = "Salva um produto",
+            description = """
+                    A chamada deste endpoint salva um produto
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salva um produto"),
+            @ApiResponse(responseCode = "500", description = "Erro nas informações inseridas")
+    })
     @PostMapping
     public ProductDto createProduct(@RequestBody ProductDto ProductDto) {
         Product product = CreateProductUseCase.create(ProductDto.name(), ProductDto.description(), ProductDto.sku(), ProductDto.price());
@@ -42,6 +80,15 @@ public class ProductController {
         return new ProductDto(productJpaGateway.create(product));
     }
 
+    @Operation(
+            summary = "Atualiza produto por código",
+            description = """
+                    A chamada deste endpoint edita um produto a partir de um id
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Editar um produto a partir do ID"),
+            @ApiResponse(responseCode = "500", description = "Erro nas informações inseridas")
+    })
     @PutMapping("/{id}")
     public ProductDto updateProduct(@PathVariable("id") Long productId, @RequestBody ProductDto newProduct) {
         Product product = productJpaGateway.findById(productId);
@@ -50,6 +97,15 @@ public class ProductController {
         return new ProductDto(productJpaGateway.update(updatedProduct));
     }
 
+    @Operation(
+            summary = "Exclusão permanente de produto por código",
+            description = """
+                    A chamada deste endpoint exclui permanentemente um produto a partir de um id
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Exclui permanentemente um produto a partir do ID"),
+            @ApiResponse(responseCode = "500", description = "Erro nas informações inseridas")
+    })
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable("id") Long productId) {
         Product product = productJpaGateway.findById(productId);
